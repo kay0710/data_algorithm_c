3e29e651875ee6cb6741464f499427ad8bc30a10
Kay <100210846+kay0710@users.noreply.github.com>
2022-07-18 21:35:23 +0900

    Add files via upload

---
+++ 1_4_q12_milti_table2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int row, col;
+
+	for (row = 1; row < 10; row++)
+	{
+		if (row == 1)
+		{
+			printf("   |");
+			for (col = 1; col < 10; col++)
+			{
+				printf("% 3d", row * col);
+			}
+			printf("\n---+---------------------------\n");
+		}
+		for (col = 1; col < 10; col++)
+		{
+			if (col == 1)
+			{
+				printf(" %d |", row * col);
+			}
+			printf("% 3d", row * col);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
